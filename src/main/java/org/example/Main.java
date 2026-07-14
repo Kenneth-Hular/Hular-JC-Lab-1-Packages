e4608fa5d938940c4e325be20423440eb4b9f0e1
Kenneth Hular <kenneth.hular@gcash.com>
2026-07-14 11:25:45 +0800

    Lab 1 Packages

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import org.example.packageone.ClassOne;
+import org.example.packagethree.ClassThree;
+import org.example.packagetwo.ClassTwo;
+
 
 // Import here manually
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -7,6 +11,13 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        ClassOne one = new ClassOne();
+        ClassTwo two = new ClassTwo();
+        ClassThree three = new ClassThree();
+
+        System.out.println(one.greet());
+        System.out.println(two.greet());
+        System.out.println(three.greet());
 
     }
 }
